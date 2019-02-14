a2e0fcc366c907f3b5afc67e339df944d417da3a
SpencerD1 <spencerwdickinson@gmail.com>
2019-02-14 16:43:15 -0500

    added CSVs and comments

--- src/Tester.java
+++ src/Tester.java
@@ -28,115 +28,128 @@ public class Tester {
 		return data;
 
 	}
+
 	public static void main(String[] args) {
+
+		//1. instantiate variables: random, pop array, map of data, training lists
 		Random rand = new Random();
 		ArrayList<Tree> trees = new ArrayList<Tree>();
 		HashMap<Double, Tree> errors = new HashMap<Double, Tree>();
 		ArrayList<Double> e = new ArrayList<Double>();
 		ArrayList<Double> training = new ArrayList<Double>();
 		ArrayList<Double> testing = new ArrayList<Double>();
+
+		//2. create intial trees
 		for(int i = 0; i < 1000; i++) {
 			trees.add(new Tree(3));
 		}
+
 		ArrayList<Double> keys = new ArrayList<Double>();
 		HashMap data = readXYFromCSV("dataset1.csv");
 
+		//3. divide up data into training and testing
+
+		//From Paper, can CITE
 		keys.addAll(data.keySet());
 		Collections.sort(keys);
-		for(int x = 0; x < 1; x++) {
-			while(training.size() < 701) {
-				int index = rand.nextInt(1002);
-				if(!training.contains(keys.get(index))) {
-					training.add(keys.get(index));
-				}
+
+		while(training.size() < 701) {
+			int index = rand.nextInt(1002);
+			if(!training.contains(keys.get(index))) {
+				training.add(keys.get(index));
 			}
-			while(testing.size() < 301) {
-				int index = rand.nextInt(1002);
-				if(!testing.contains(keys.get(index))&&!training.contains(keys.get(index))) {
-					testing.add(keys.get(index));
-				}
+		}
+		while(testing.size() < 301) {
+			int index = rand.nextInt(1002);
+			if(!testing.contains(keys.get(index))&&!training.contains(keys.get(index))) {
+				testing.add(keys.get(index));
 			}
+		}
 
+		//4. find all mean Squared errors, and put into errors map, with squared error as key
+		for(Tree t: trees) {
+			errors.put(t.meanSquaredError(data, training),t);
+		}
+		ArrayList<Tree> mutations = new ArrayList<Tree>();
+		ArrayList<Tree> crossovers = new ArrayList<Tree>();
+		int gen = 0;
+		double best = 100000000;
 
+		//5. keep making new generations until squared error below .5, or # of gens hits 200
+		while(!(best < .5 || gen >= 200)) {
+			trees = new ArrayList<Tree>();
 
-			for(Tree t: trees) {
-				errors.put(t.meanSquaredError(data, training),t);
-			}
-			ArrayList<Tree> mutations = new ArrayList<Tree>();
-			ArrayList<Tree> crossovers = new ArrayList<Tree>();
-			int gen = 0;
-			double best = 100000000;
+			//1. get top 100 trees in population, reproduce (trees), and add to crossovers and mutations
+			for(int t = 0; t < 100; t++) {
+				Tree t1 = errors.get(Collections.min(errors.keySet()));
+				errors.remove(Collections.min(errors.keySet()));
+				crossovers.add(t1);
+				mutations.add(t1);
 
-
-			while(!(best < .5 || gen >= 200)) {
-				trees = new ArrayList<Tree>();
-
-				for(int t = 0; t < 100; t++) {
-					Tree t1 = errors.get(Collections.min(errors.keySet()));
-					errors.remove(Collections.min(errors.keySet()));
-					crossovers.add(t1);
-					mutations.add(t1);
-
-					trees.add(t1);
-
-				}
-				//System.out.println("r " + trees.size());
-				for(int i = 0; i < 40; i++) {
-					Tree t1 = new Tree(Tree.copy(crossovers.get(i).myRoot));
-					for(int j = 0; j <10; j++) {
-						Tree t2 = new Tree(Tree.copy(crossovers.get(j).myRoot));
-
-						Tree.crossover(t1, t2);
-						t1.depthLimit(12);
-						t2.depthLimit(12);
-						trees.add(t1);
-						trees.add(t2);
-
-
-					}
-				}
-				//System.out.println("c " +trees.size());
-
-				for(int k = 0; k < 10; k++) {
-					for(int m = 0; m < 10; m++) {
-						Tree t = new Tree(Tree.copy(mutations.get(m).myRoot));
-
-						Tree.mutate(t);
-
-						trees.add(t);
-					}
-				}
-
-				//System.out.println("m "+trees.size());
-
-				best = Collections.min(errors.keySet());
-				errors = new HashMap<Double, Tree>();
-				//System.out.println(trees.size());
-				for(Tree t: trees) {
-					//t.printTree();
-					errors.put(t.meanSquaredError(data, training),t);
-				}
-
-
-				gen++;
-
-
-				mutations = new ArrayList<Tree>();
-				crossovers = new ArrayList<Tree>();
+				trees.add(t1);
 
 			}
-			Tree t = errors.get(Collections.min(errors.keySet()));
-			System.out.println("squared "+Collections.min(errors.keySet()));
-			System.out.println("mean "+t.meanError(data, training));
-			System.out.println(gen);
-			System.out.println("squared "+t.meanSquaredError(data, testing));
-			System.out.println("mean "+t.meanError(data, testing));
-			System.out.println(t.evaluate(t.myRoot, 1.22));
-			System.out.println(t.evaluate(t.myRoot, -4.38));
+			//System.out.println("r " + trees.size());
+
+		//2. cross the top 40 trees, and cross each with the best 10
+		for(int i = 0; i < 40; i++) {
+			Tree t1 = new Tree(Tree.copy(crossovers.get(i).myRoot));
+			for(int j = 0; j <10; j++) {
+				Tree t2 = new Tree(Tree.copy(crossovers.get(j).myRoot));
+
+				Tree.crossover(t1, t2);
+				t1.depthLimit(12);
+				t2.depthLimit(12);
+				trees.add(t1);
+				trees.add(t2);
 
 
+			}
+		}
+		//System.out.println("c " +trees.size());
+
+		//3. Mutate the top 10 trees, each tree 10 seperate times -> 100 trees
+		for(int k = 0; k < 10; k++) {
+			for(int m = 0; m < 10; m++) {
+				Tree t = new Tree(Tree.copy(mutations.get(m).myRoot));
+
+				Tree.mutate(t);
+
+				trees.add(t);
+			}
+		}
+
+		//System.out.println("m "+trees.size());
+
+		//currently have 500 trees (100 reproductions, 300 cross, 100 mutate)
+
+		//4. clear error map, so can put in new trees
+		errors = new HashMap<Double, Tree>();
+		//System.out.println(trees.size());
+		for(Tree t: trees) {
+			//t.printTree();
+			errors.put(t.meanSquaredError(data, training),t);
+		}
+		//6. find best tree in new pop
+		best = Collections.min(errors.keySet());
+
+		gen++;
+
+		//7. clear mutations and cross overs
+		mutations = new ArrayList<Tree>();
+		crossovers = new ArrayList<Tree>();
 
 		}
-	}
 
+	//6. print out best tree
+	Tree t = errors.get(Collections.min(errors.keySet()));
+	System.out.println("squared "+Collections.min(errors.keySet()));
+	System.out.println("mean "+t.meanError(data, training));
+	System.out.println(gen);
+	System.out.println("squared "+t.meanSquaredError(data, testing));
+	System.out.println("mean "+t.meanError(data, testing));
+	System.out.println(t.evaluate(t.myRoot, 1.22));
+	System.out.println(t.evaluate(t.myRoot, -4.38));
+
+	}
 }
